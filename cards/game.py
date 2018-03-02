d7848851ce76ba6f763b4ec6f719dbd554b25ed2
Villiers Strauss <villiers.strauss@regiohelden.de>
2018-03-02 15:55:59 +0100

    add numbers to card

--- cards/game.py
+++ cards/game.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals, print_function
 from functools import total_ordering
 
+
 @total_ordering
 class Suit(object):
     def __lt__(self, other):
@@ -18,7 +19,48 @@ hearts = Suit()
 SUIT_ORDER = [spades, clubs, diamonds, hearts]
 
 
+@total_ordering
+class Number(object):
+    def __lt__(self, other):
+        return NUMBER_ORDER.index(self) < NUMBER_ORDER.index(other)
+
+    def __eq__(self, other):
+        return self is other
+
+
+two = Number()
+three = Number()
+four = Number()
+five = Number()
+six = Number()
+seven = Number()
+eight = Number()
+nine = Number()
+ten = Number()
+jack = Number()
+queen = Number()
+king = Number()
+ace = Number()
+
+NUMBER_ORDER = [
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+    ten,
+    jack,
+    queen,
+    king,
+    ace,
+]
+
+
 class Card(object):
 
-    def __init__(self, suit):
+    def __init__(self, suit, number):
         self.suit = suit
+        self.number = number
--- cards/test.py
+++ cards/test.py
@@ -9,8 +9,29 @@ from . import game
 class GameTestCase(TestCase):
 
     def test_has_suit(self):
-        card = game.Card(game.hearts)
+        card = game.Card(game.hearts, game.jack)
         self.assertIs(card.suit, game.hearts)
+
+    def test_has_number(self):
+        card = game.Card(game.hearts, game.jack)
+        self.assertIs(card.number, game.jack)
+
+
+class NumberTestCase(TestCase):
+
+    def test_number_order(self):
+        self.assertLess(game.two, game.three)
+        self.assertLess(game.three, game.four)
+        self.assertLess(game.four, game.five)
+        self.assertLess(game.five, game.six)
+        self.assertLess(game.six, game.seven)
+        self.assertLess(game.seven, game.eight)
+        self.assertLess(game.eight, game.nine)
+        self.assertLess(game.nine, game.ten)
+        self.assertLess(game.ten, game.jack)
+        self.assertLess(game.jack, game.queen)
+        self.assertLess(game.queen, game.king)
+        self.assertLess(game.king, game.ace)
 
 
 class SuitTestCase(TestCase):
