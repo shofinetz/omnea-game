66d4af922fe1056cd89d013c1937260e0840a70a
Villiers Strauss <villiers.strauss@regiohelden.de>
2018-03-02 15:27:25 +0100

    add card class

--- cards/game.py
+++ cards/game.py
@@ -1,2 +1,6 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals, print_function
+
+
+class Card(object):
+    pass
--- cards/test.py
+++ cards/test.py
@@ -1,2 +1,12 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals, print_function
+
+from unittest import TestCase
+
+from .game import Card
+
+
+class GameTestCase(TestCase):
+
+    def test_has_suit(self):
+        card = Card()
