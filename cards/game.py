aa393d26357e0403f720b321779780132a6cafed
Villiers Strauss <villiers.strauss@regiohelden.de>
2018-03-02 15:46:32 +0100

    add suits

--- cards/game.py
+++ cards/game.py
@@ -1,5 +1,21 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals, print_function
+from functools import total_ordering
+
+@total_ordering
+class Suit(object):
+    def __lt__(self, other):
+        return ORDER.index(self) < ORDER.index(other)
+
+    def __eq__(self, other):
+        return self is other
+
+
+spades = Suit()
+clubs = Suit()
+diamonds = Suit()
+hearts = Suit()
+ORDER = [spades, clubs, diamonds, hearts]
 
 
 class Card(object):
--- cards/test.py
+++ cards/test.py
@@ -3,10 +3,18 @@ from __future__ import unicode_literals, print_function
 
 from unittest import TestCase
 
-from .game import Card
+from . import game
 
 
 class GameTestCase(TestCase):
 
     def test_has_suit(self):
-        card = Card()
+        card = game.Card()
+
+
+class SuitTestCase(TestCase):
+
+    def test_suit_order(self):
+        self.assertLess(game.spades, game.clubs)
+        self.assertLess(game.clubs, game.diamonds)
+        self.assertLess(game.diamonds, game.hearts)
